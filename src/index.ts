765ec8da4469077b50a39777c1e168771eeaf403
泉户泽雨 <1624910218@qq.com>
2024-09-21 14:24:16 +0800

    refactor(canvas): 使用tab缩进和更新构建脚本
    - 更新`.editorconfig`以使用tab缩进和更大的缩进大小。
    - 新增构建和清理脚本，优化包管理和脚本执行。
    - 在`package.json`中添加脚本来管理构建、清理、发布流程。
    - 更新`index.ts`以包含新的服务类和导入语句。

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
-import { Context, Schema } from 'koishi'
+import { Awaitable, Context, Schema, Service } from 'koishi'
+import { } from "@quanhuzeyu/koishi-plugin-node"
+import type obj_canvas from "canvas"
+import type { } from "canvas"
+
+export const inject = {
+	required: ['node']
+}
 
 export const name = 'canvas'
+export const usage = `
+通过obj_canvas访问完整canvas功能
+`
 
-export interface Config {}
+export interface Config {
+	canvasVersion: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+	canvasVersion: Schema.string().default('2.11.2').description('canvas 版本')
+})
+
+declare module 'koishi' {
+	interface Context {
+		canvas: typeof obj_canvas
+	}
+}
+
+export class QhzyCanvas extends Service {
+
+	constructor(ctx: Context, config: Config) {
+		super(ctx, 'canvas')
+		this.config = {
+			...config
+		}
+	}
+
+	protected async start() {
+		await this.ctx.node.safeImport<typeof obj_canvas>('canvas')
+			.then(obj_canvas => {
+				this.ctx.canvas = obj_canvas
+			})
+			.catch(e => {
+				this.ctx.logger.info(`加载 canvas 失败: ${e}`)
+			})
+	}
+}
 
 export function apply(ctx: Context) {
-  // write your plugin here
+	ctx.plugin(QhzyCanvas)
 }
